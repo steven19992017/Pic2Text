971d3aac9c2dea51acd0a179b97c3c196bd3c4f9
Steven Sun <hsun03@calpoly.edu>
2019-05-12 04:24:06 -0700

    updates from original project

---
+++ Pic2TextKeyboard/Pic2TextKeyboard.Android/CameraActivity.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Gms.Vision;
+using Android.Gms.Vision.Texts;
+using Android.Graphics;
+using Android.Net;
+using Android.OS;
+using Android.Provider;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Java.IO;
+
+namespace Pic2TextKeyboard.Droid
+{
+    [Activity(Label = "CameraActivity")]
+    public class CameraActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.CameraLayout);
+
+            if (IsThereAnAppToTakePictures())
+                TakeAPicture(null, new System.EventArgs());
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            string result = string.Empty;
+
+            try
+            {
+                if (requestCode == 100 && resultCode == Result.Ok)
+                {
+                    if (data != null && data.Extras != null)
+                    {
+                        Bitmap imageBitmap = (Bitmap)data.Extras.Get("data");
+                        TextRecognizer textRecognizer = new TextRecognizer.Builder(ApplicationContext).Build();
+                        Frame frame = new Frame.Builder().SetBitmap(imageBitmap).Build();
+                        SparseArray items = textRecognizer.Detect(frame);
+                        StringBuilder builder = new StringBuilder();
+
+                        for (int i = 0; i < items.Size(); i++)
+                        {
+                            TextBlock item = (TextBlock)items.ValueAt(i);
+                            builder.Append(item.Value);
+                            builder.Append(" ");
+                        }
+
+                        result = builder.ToString();
+                    }
+                }
+            }
+            catch (System.Exception e)
+            {
+                Toast.MakeText(this, e.Message, ToastLength.Long).Show();
+            }
+
+            TextData.LastText = result;
+            Finish();
+        }
+
+        private bool IsThereAnAppToTakePictures()
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            IList<ResolveInfo> availableActivities =
+                PackageManager.QueryIntentActivities(intent, PackageInfoFlags.MatchDefaultOnly);
+            return availableActivities != null && availableActivities.Count > 0;
+        }
+
+        private void TakeAPicture(object sender, System.EventArgs eventArgs)
+        {
+            try
+            {
+                StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                StrictMode.SetVmPolicy(builder.Build());
+                Intent intent = new Intent(MediaStore.ActionImageCapture);
+                if (intent.ResolveActivity(PackageManager) != null)
+                {
+                    StartActivityForResult(intent, 100);
+                }
+            }
+            catch (System.Exception e)
+            {
+                Toast.MakeText(this, e.Message, ToastLength.Long).Show();
+            }
+        }
+    }
+}
---
+++ Pic2TextKeyboard/Pic2TextKeyboard.Android/MainActivity.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.InputMethodServices;
+using Java.Lang;
+using Android.Views.InputMethods;
+using Android.Content;
+
+namespace Pic2TextKeyboard.Droid { 
+
+    [Service(Permission = "android.permission.BIND_INPUT_METHOD", Label = "Pic2TextKeyboard")]
+    [MetaData("android.view.im", Resource = "@xml/method")]
+    [IntentFilter(new string[] { "android.view.InputMethod" })]
+    public class MainActivity : InputMethodService, KeyboardView.IOnKeyboardActionListener
+    {
+        public static MainActivity Instance { get; private set; }
+
+        private KeyboardView kv;
+        private Keyboard keyboard;
+        private IInputConnection ic;
+
+        public MainActivity()
+        {
+            Instance = this;
+        }
+
+        public override void OnStartInput(EditorInfo attribute, bool restarting)
+        {
+            base.OnStartInput(attribute, restarting);
+
+            if (!string.IsNullOrEmpty(TextData.LastText))
+            {
+                CurrentInputConnection.CommitText(TextData.LastText, 1);
+                TextData.LastText = string.Empty;
+            }
+        }
+
+        public override View OnCreateInputView()
+        {
+            kv = (KeyboardView)LayoutInflater.Inflate(Resource.Layout.Keyboard, null);
+            keyboard = new Keyboard(this, Resource.Xml.Qwerty);
+            kv.Keyboard = keyboard;
+            kv.OnKeyboardActionListener = this;
+            return kv;
+        }
+
+        public void OnKey([GeneratedEnum] Android.Views.Keycode primaryCode, [GeneratedEnum] Android.Views.Keycode[] keyCodes)
+        {
+            ic = CurrentInputConnection;
+
+            if (ic == null)
+                return;
+
+            switch (primaryCode)
+            {
+                case Android.Views.Keycode.Del:
+                    string selectedText = ic.GetSelectedText(GetTextFlags.None);
+
+                    if (string.IsNullOrEmpty(selectedText))
+                        ic.DeleteSurroundingText(1, 0);
+                    else
+                        ic.CommitText("", 1);
+
+                    break;
+                case Android.Views.Keycode.Unknown:
+                    TextData.LastText = string.Empty;
+                    Intent intent = new Intent();
+                    intent.SetClass(this, typeof(CameraActivity));
+                    intent.SetFlags(ActivityFlags.NewTask);
+                    StartActivity(intent);
+                    break;
+                default:
+                    char code = (char)primaryCode;
+                    ic.CommitText(code.ToString(), 1);
+                    break;
+            }
+        }
+
+        public void OnPress([GeneratedEnum] Android.Views.Keycode primaryCode)
+        {
+        }
+
+        public void OnRelease([GeneratedEnum] Android.Views.Keycode primaryCode)
+        {
+        }
+
+        public void OnText(ICharSequence text)
+        {
+        }
+
+        public void SwipeDown()
+        {
+        }
+
+        public void SwipeLeft()
+        {
+        }
+
+        public void SwipeRight()
+        {
+        }
+
+        public void SwipeUp()
+        {
+        }
+    }
+}
